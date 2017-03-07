c3d229e7905823a68fa70121c8e6522b2e81093a
ssaigusa <ssaigusa@techfirm.co.jp>
2017-03-07 18:33:18 +0900

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -1,11 +1,23 @@
+function setHeight() {
+    // ブラウザのの高さを取得 更にヘッダー分だけ縮小
+    let hsize = $(window).height();
+    $(".contents-top").css("height", hsize / 2 + "px");
+    $(".contents-top").css("margin-top", hsize / 2 - 50 + "px");
+}
+
 $(function () {
     $('.button-collapse').sideNav({
         menuWidth: 300, // Default is 300
         edge: 'right', // Choose the horizontal origin
         closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
         draggable: true // Choose whether you can drag to open on touch screens
-    }
-    );
+    });
+
+    setHeight();    
+    // ウィンドウ変更時も同様の処理
+    $(window).resize(function () {
+        setHeight();
+    });
 
     $(window).on("load scroll", function () {
         //ヘッダーの高さ分(80px)スクロールするとfixedクラスを追加
