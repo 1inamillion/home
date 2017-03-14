0d89aa55d1cc5e265398effac3df9d3575eb64a2
ssaigusa <ssaigusa@techfirm.co.jp>
2017-03-14 17:11:21 +0900

    スクロール実装

--- js/home.js
+++ js/home.js
@@ -1,10 +1,36 @@
+
 $(function () {
+    const NEWS_POS = $(".contents-news").offset().top - 70;
+    const DISCO_POS = $(".contents-disco").offset().top - 70;
+    const CONTACT_POS = $(".contents-contact").offset().top - 70;
+    
     // サイドバーのトリガー
     $('.button-collapse').sideNav({
         menuWidth: 300,
         edge: 'right',
         closeOnClick: true,
         draggable: true
+    });
+
+    $("#side_news").click(function () {
+        $("html,body").animate(
+            { scrollTop: NEWS_POS },
+            { queue: false }
+        );
+    });
+
+    $("#side_disco").click(function () {
+        $("html,body").animate(
+            { scrollTop: DISCO_POS },
+            { queue: false }
+        );
+    });
+
+    $("#side_contact").click(function () {
+        $("html,body").animate(
+            { scrollTop: CONTACT_POS },
+            { queue: false }
+        );
     });
 
     $(".fixed-action-btn").hide();
